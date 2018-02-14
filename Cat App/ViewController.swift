151c622e53b7c122fb233d2672be4e9312faaa7b
Andy Harris <andy.harris@live.co.uk>
2018-02-14 12:42:11 +0000

    Test commit

--- Cat App/ViewController.swift
+++ Cat App/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var answerField: UILabel!
+    
+    @IBOutlet weak var ageText: UITextField!
+    
+    // Age is in text. Need to convert to an integer
+    @IBAction func getAge(_ sender: Any) {
+        
+        if let ageEntered = ageText.text{
+            
+            if let ageAsNumber = Int(ageEntered){
+                
+                let ageInCatYears = ageAsNumber * 7
+                answerField.text = "Your cat is " + String(ageInCatYears) + " in cat years"
+            } else{
+                answerField.text = "Please enter a number"
+            
+            }
+        } else{
+            answerField.text = "Please enter your cat's actual age first"
+        }
+        
+    }
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
